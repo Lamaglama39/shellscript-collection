b7a576dd1aa26cd626af043e0f4cafcf36c3c4d9
Namako <kanikama775@gmail.com>
2022-08-28 22:39:02 +0900

    add image_bash $tofile,$todir,$resize

--- image_bash/image_bash.sh
+++ image_bash/image_bash.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
 for afile in $(ls *.jpg);do
-  tofile="resize_${afile}"
+  todir='格納先フォルダ'
+  tofile="リサイズ先ファイル名"
+  todir_tofile="$todir$tofile"
+  resize='20%'
+
   echo $afile $tofile
-  "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe" convert $afile -resize 20% $tofile
+  "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe" convert $afile -resize $resize $todir_tofile
 
 done
