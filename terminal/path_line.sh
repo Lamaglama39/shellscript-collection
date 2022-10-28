a531164a01f0611f421f354543f03759dcf651e3
local_ubuntu <kanikama775@gmail.com>
2022-10-28 23:42:11 +0900

    :recycle: Add header
    Added headers to path_line.sh and retrycp.sh describing usage.

--- terminal/path_line.sh
+++ terminal/path_line.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#########################################################################################
+# Scrip tName  : path_line.sh
+# Discription  : The values set in the PATH environment variable are output line by line.
+#                It does nothing else...
+# How to : path_line.sh
+#########################################################################################
 
-echo -e ${PATH//:/'\n'} | sort
-echo -e ${PATH//:/'\n'} | wc -c
+echo -e "${PATH//:/'\n'}" | sort
+echo -e "${PATH//:/'\n'}" | echo 'Total: '"$(wc -l)"
--- terminal/retrycp.sh
+++ terminal/retrycp.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+############################################################################
+# Scrip tName  : retrycp.sh
+# Discription  : If copying fails, 
+#                copying is performed again after a certain period of time.
+# How to : retrycp.sh $1 $2
+#     $1 : original file
+#     $2 : File after copy
+############################################################################
 
-until cp $1 $2; do
-  echo 'Attempt to copy failed. waiting...'
-  sleep 5
+until cp "$1" "$2" 2> /dev/null; do
+  echo -n 'Failed to copy files, will rerun in 5 seconds'
+  for time in {0..5}
+  do
+    if [[ $time -eq 5 ]]; then
+      echo "."
+      sleep 1
+    else
+      sleep 1
+      echo -n "."
+    fi
+  done
 done
