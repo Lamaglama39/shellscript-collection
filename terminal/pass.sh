fad1d8a6b22de65a0c65110b6a1a9291fd6c4569
Thinkpad user <kanikama775@gmail.com>
2022-10-30 21:13:57 +0900

    :recycle: Add Header,Add corresponding argument to pass.sh
    Added header describing usage.
    Changed to support multiple arguments.

--- terminal/pass.sh
+++ terminal/pass.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+###########################################################################
+# Scrip tName  : pass.sh
+# Discription  : Outputs users corresponding to the specified user ID.
+# How to : pass.sh $1 $2 ...
+#     $1 : User ID
+#     $2 : User ID
+###########################################################################
 
 for i in $( cut -f 1,3 -d: /etc/passwd ) ; do
   array[${i#*:}]=${i%:*}
 done
 
-echo "User ID $1 is ${array[$1]}."
+
+for id in "$@"; do
+  if [[ `cut -f 3 -d: /etc/passwd | grep $id` ]]; then
+    echo "User ID $id is ${array[$id]}."
+  else
+    echo "ERROR: ID $id "'does not exist.'
+  fi
+done
+
 echo "There are currently ${#array[@]} user accounts on the system."
