d8e5ca4ecbde35e86e4fed9f30b33f9adb336238
local_ubuntu <kanikama775@gmail.com>
2022-11-05 12:45:00 +0900

    :tada: Add script to do URL encoding.
    url_encode.sh is a program that,
    URL encoding of a file string passed from a pipe.

---
+++ conversion/url_encode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#################################################################
+# Scrip tName  : encode.sh
+# Discription  : URL encoding of the content of the passed file.
+# How to : cat $filename | encode.sh
+#################################################################
+set -euo pipefail
+
+url_encode() {
+  while read -r line
+  do
+    echo "$line" |
+      nkf -W8MQ |
+      sed 's/=$//' |
+      tr '=' '%' |
+      paste -s -d '\0' - |
+      sed -e 's/%7E/~/g' \
+          -e 's/%5F/_/g' \
+          -e 's/%2D/-/g' \
+          -e 's/%2E/./g'
+  done
+}
+
+url_encode
