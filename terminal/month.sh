c483083013f87163cf347b992e2fa7d255de83bd
local_ubuntu <kanikama775@gmail.com>
2022-11-09 23:59:22 +0900

    :tada: Added a script that converts the month's English notation to numbers.
    Converting English notation of months to numbers in a file passed via pipe.
    Mainly used for log files.

---
+++ terminal/month.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#########################################################################
+# Scrip tName  : month.sh
+# Discription  : Convert the English notation of the month into numbers.
+# How to : cat /var/log/example | month.sh
+#########################################################################
+
+ sed 's/Jan/01/' |
+ sed 's/Feb/02/' |
+ sed 's/Mar/03/' |
+ sed 's/Apr/04/' |
+ sed 's/May/05/' |
+ sed 's/Jun/06/' |
+ sed 's/Jul/07/' |
+ sed 's/Aug/08/' |
+ sed 's/Sep/09/' |
+ sed 's/Oct/10/' |
+ sed 's/Nov/11/' |
+ sed 's/Dec/12/'
