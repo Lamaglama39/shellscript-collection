c469ba26b7f988325622ac98f03c665d3ea79933
local_ubuntu <kanikama775@gmail.com>
2022-11-12 12:59:33 +0900

    :+1: Added the ability to specify file formats.
    argument specifies the output file format.

--- conversion/barcode.sh
+++ conversion/barcode.sh
@@ -15,6 +15,45 @@ set -euo pipefail
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd $script_dir
 
-num=$1
-echo "$num" | barcode -E > "$num".eps
-convert "$num".eps "$num".png
+while getopts epjgn:f: option
+do
+  case $option in
+    e)
+      format=eps;;
+    p)
+      format=pdf;;
+    j)
+      format=jpeg;;
+    g)
+      format=png;;
+    n)
+      num=$OPTARG;;
+    f)
+      filename=$OPTARG;;
+    \?)
+      echo "Usage: $0 [-e(eps)] [-p(pdf)] [-j(jpeg)] [-g(png)] [-n number] [-f filename]" 1 >&2
+      exit 1;;
+  esac
+done
+shift $(expr "$OPTIND" - 1)
+if [ $# -ge 1 ]; then
+  echo 'ERROR:Non-optional arguments'"$*"
+else
+  echo 'SUCCESS:Correct arguments were specified.'
+fi
+
+echo "$num" | barcode -E > "${filename:-$num}".eps
+if [[ $format == 'eps' ]]; then
+  :
+elif [[ $format == 'pdf' ]]; then
+  convert "${filename:-$num}".eps "${filename:-$num}".pdf
+  rm "${filename:-$num}".eps
+elif [[ $format == 'jpeg' ]]; then
+  convert "${filename:-$num}".eps "${filename:-$num}".jpeg
+  rm "${filename:-$num}".eps
+elif [[ $format == 'png' ]]; then
+  convert "${filename:-$num}".eps "${filename:-$num}".png
+  rm "${filename:-$num}".eps
+else
+:
+fi
